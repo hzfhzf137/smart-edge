0fd674da6d41ca9a20618e8533c4a43c26730fae
hzfhzf137 <hzfhzf137@gmail.com>
2025-01-28 23:53:40 +0500

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <h1 className=' bg-gray-500 text-blue-400 text-2xl'>this is my first heading</h1>
+    </>
+  )
+}
+
+export default App

4e07bb19b1e90e26cfeb70e063f40c69b070a11e
hzfhzf137 <hzfhzf137@gmail.com>
2025-01-29 00:08:44 +0500

    Updated Vite config for GitHub Pages deployment

--- vite.config.js
+++ vite.config.js
@@ -2,8 +2,8 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
-
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(),    tailwindcss()  ],
+  base: '/smart-edge/',  // Set the base path to your repository name
+  plugins: [react(), tailwindcss()],
 })

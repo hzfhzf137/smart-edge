fb7916650e0ff30b26f747a3df2103db20b57355
hzfhzf137 <hzfhzf137@gmail.com>
2025-02-25 22:15:41 +0500

    airpods.jsx
    airpods page height set dynamically.
    page scroll to top added

--- src/pages/airpods/airpods.jsx
+++ src/pages/airpods/airpods.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Navbar from "../reuseableComponents/navbar";
 import Footer from "../reuseableComponents/footer";
 import AirpodsHero from "./components/airpodsHero";
@@ -6,10 +6,26 @@ import AirpodsFeaturesAndGallery from "./components/airpodsFeaturesAndGallery";
 import AirpodsAddToCart from "./components/airpodsAddToCart";
 
 const Airpods = () => {
+  const [navHeight, setNavHeight] = useState(0);
+
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, []);
+
+  useEffect(() => {
+    const navbar = document.querySelector("nav");
+    if (navbar) {
+      setNavHeight(navbar.offsetHeight);
+    }
+  }, []);
+
   return (
     <React.Fragment>
       <Navbar />
-      <div className="pt-16 pb-40 overflow-x-hidden">
+      <div 
+        className="overflow-x-hidden pb-40"
+        style={{ paddingTop: navHeight }}
+      >
         <AirpodsHero />
         <AirpodsFeaturesAndGallery />
         <AirpodsAddToCart />
